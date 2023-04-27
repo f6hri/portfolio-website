359ed5f598e176d90b63f9576e13407b13ffb3e4
Fahri Muhammet Demir <76499302+f6hri@users.noreply.github.com>
2023-04-27 13:47:51 +0300

    Update about.js

--- config/about.js
+++ config/about.js
@@ -1,3 +1,3 @@
 export default [
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe odit aliquam ducimus inventore itaque ullam id similique accusantium nam? Magnam, possimus sit. Aperiam rerum accusantium maiores voluptatum deleniti expedita nam."
+    "Selam, ben fahri. Web geliştirmeye ortaokul son sınıfta başladım ve o zamandan beri bu alana tutkuyla bağlıyım. Frontend için React JS ve Vue JS, backend geliştirme için Node js kullanarak modern ve kullanıcı dostu web siteleri tasarlıyorum ve kodluyorum. Şu ana kadar portofilo sayfaları, blog sayfaları, film sitesi gibi çeşitli projeler geliştirdim. Web geliştirme alanında kendimi sürekli geliştirmek ve yeni teknolojiler öğrenmek istiyorum. Eğer sizin de web sitenize ihtiyacınız varsa, benimle iletişime geçebilirsiniz."
 ]
